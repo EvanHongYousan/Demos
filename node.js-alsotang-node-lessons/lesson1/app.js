6083ced3cd99bdbb645ba8628fd5f695f3c57241
Evan Yan <1370204201@qq.com>
2015-08-05 17:40:30 +0800

    toy code commit

--- node.js-alsotang-node-lessons/lesson1/app.js
+++ node.js-alsotang-node-lessons/lesson1/app.js
@@ -5,8 +5,8 @@ var express = require('express');
 
 app = express();
 
-app.get('/',function(request, response){
-    response.send("fdsa");
-}).listen(3000,function(){
-    console.log('app is listening at port 3000');
+app.get('/', function(request, response) {
+	response.send("fdsa");
+}).listen(3000, function() {
+	console.log('app is listening at port 3000');
 });
--- node.js-alsotang-node-lessons/lesson3/index.js
+++ node.js-alsotang-node-lessons/lesson3/index.js
@@ -7,25 +7,25 @@ var superagent = require('superagent');
 
 var app = express();
 
-app.get('/',function(request, response){
+app.get('/', function(request, response) {
     superagent.get('https://cnodejs.org/')
-        .end(function(err, sres){
-            if(err){
+        .end(function(err, sres) {
+            if (err) {
                 return next(err);
             }
             var $ = cheerio.load(sres.text);
             var items = [];
-            $("#topic_list .topic_title").each(function(index,element){
+            $("#topic_list .topic_titinstale").each(function(index, element) {
                 var $element = $(element);
                 items.push({
-                    title:$element.attr('title'),
-                    href:$element.attr('href')
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
                 });
             });
         })
 });
 
 var a = 1;
-app.listen(3000,function(){
+app.listen(3000, function() {
     console.log("app is running at port 3000");
 });
