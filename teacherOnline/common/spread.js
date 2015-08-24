3b82b126d9f072ff401f407a3dae72df0b2c58f3
Evan Yan <1370204201@qq.com>
2015-08-24 16:42:32 +0800

    红包活动页修改

--- teacherOnline/common/spread.js
+++ teacherOnline/common/spread.js
@@ -2,10 +2,12 @@
  * Created by yantianyu on 15/8/20.
  */
 var spreadData = {
-	id : '55d552f82fa2a328c3013097',
-	name:"暑期红包活动",
-	pic : 'http://192.168.0.233:8088/rtc/spread/share/128e38814f7a47458fb83310a615bbe7.png',
-	shareWording : '快来抢吧',
-	jsonPURL : 'http://192.168.0.231/app/spread/activity',
-	shareURL : 'http://192.168.0.233:8088/rtc/spread/55d552f82fa2a328c3013097/share/index.html'
+    id: '55d58fb60cf24143181607e0',
+    name: "九月一号开学抢红包活动",
+    pic: 'http://ftp.ztmomo.com/rtc/spread/share/67d784b9d1af44ae8c4e2d7e6ae7ffdc.png',
+    shareWording: '快来抢100万提问券！作业堆成山也不用怕，有呼叫老师，随时问，立刻答，包你懂！',
+    jsonPURL: 'http://test.hjlaoshi.com/app/spread/activity',
+    shareURL: 'http://ftp.ztmomo.com/rtc/spread/55d58fb60cf24143181607e0/share/index.html',
+    beginTime: "2015-08-20 08:27:00",
+    endTime: "2015-09-22 15:25:00"
 }
