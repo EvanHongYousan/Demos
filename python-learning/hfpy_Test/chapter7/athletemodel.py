bae76605017314746428b5ea28b10125e62e69d2
Evan Yan <1370204201@qq.com>
2015-08-26 11:10:23 +0800

    toy code commit

--- python-learning/hfpy_Test/chapter7/athletemodel.py
+++ python-learning/hfpy_Test/chapter7/athletemodel.py
@@ -4,8 +4,14 @@ from chapter7.athletelist import AthleteList
 
 
 def get_coach_data(filename):
-    pass
-
+    try:
+        with open(filename) as f:
+            data = f.read()
+            tmp = data.strip().split(',')
+        return (AthleteList(tmp.pop(0), tmp.pop(0), tmp))
+    except IOError as ioerr:
+        print('File error: ' + str(ioerr))
+        return (None)
 
 def put_to_store(file_list):
     all_athletes = {}
@@ -22,4 +28,18 @@ def put_to_store(file_list):
 
 def get_from_store():
     all_athletes = {}
+    try:
+        with open('athletes.pickle', 'rb') as athf:
+            all_athletes = pickle.load(athf)
+    except IOError as ioerr:
+        print('File error (get_from_store): ' + str(ioerr))
     return all_athletes
+
+
+the_files = ['james.txt', 'julie.txt', 'mikey.txt', 'sarah.txt']
+data = put_to_store(the_files)
+# print(data)
+for each_athlete in data:
+    # print(each_athlete)
+    # print(data[each_athlete].name + ' ' + data[each_athlete].dob)
+    print(data)
