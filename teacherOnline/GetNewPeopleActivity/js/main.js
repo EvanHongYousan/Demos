e2a0dbe2c7d061b0d36210691f9f1b0441227926
Evan Yan <1370204201@qq.com>
2015-09-15 16:38:36 +0800

    拉人活动页阶段性commit

---
+++ teacherOnline/GetNewPeopleActivity/js/main.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * Created by yantianyu on 15/9/15.
+ */
+
+$(document).ready(function(){
+    new Image().src='./images/btn_invite_hover.png';
+    $('.container').removeClass('none');
+    $('.loader').remove();
+});
