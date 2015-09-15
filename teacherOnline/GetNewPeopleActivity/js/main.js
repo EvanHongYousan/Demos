ee2188a7ddc10c5653b46f2846e023af049bfbca
Evan Yan <1370204201@qq.com>
2015-09-15 22:53:35 +0800

    活动页修改

--- teacherOnline/GetNewPeopleActivity/js/main.js
+++ teacherOnline/GetNewPeopleActivity/js/main.js
@@ -3,21 +3,33 @@
  * Created by yantianyu on 15/9/15.
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.container').removeClass('none');
     $('.loader').remove();
-    var components = (function(){
-        function maskBindEv(){
+    var components = (function () {
+        function maskBindEv() {
             $('.mask').height($(document).height());
-            $('.mask').on('touchmove',function(){
+            $('.mask').on('touchmove', function () {
                 return false;
+            });
+            $('.mask .cancel').click(function () {
+                $('.mask').addClass('none');
             });
         }
 
-        function init(){
+        function dAjax() {
+            setTimeout(function () {
+                $('.mask .dialogGotInvite').removeClass('none');
+                $('.mask').removeClass('none');
+            }, 500);
+        }
+
+        function init() {
+            dAjax();
             maskBindEv();
         }
-        return {'init':init};
+
+        return {'init': init};
     })();
     components.init();
 });
