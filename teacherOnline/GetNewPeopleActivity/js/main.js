9a97625f56b778e7c5a5de802d3be3a46c1d7247
Evan Yan <1370204201@qq.com>
2015-09-17 15:38:00 +0800

    活动页阶段性commit

--- teacherOnline/GetNewPeopleActivity/js/main.js
+++ teacherOnline/GetNewPeopleActivity/js/main.js
@@ -22,6 +22,9 @@ $(document).ready(function () {
     })();
 
     var components = (function () {
+
+        var userId = null;
+
         function maskBindEv() {
             $('.mask').height($(document).height() + 56);
             $('.mask').on('touchmove', function () {
@@ -42,7 +45,7 @@ $(document).ready(function () {
                             'scratch_paper': false
                         });
                         break;
-                    case 'shake':
+                    case 'client_msg_shake':
                         alert('摇一摇啊摇一摇');
                     default :
                         break;
@@ -57,33 +60,69 @@ $(document).ready(function () {
                     "title": '范德萨开发商打范德萨姐姐之这里是title',
                     "type": 0,
                     "image_url": 'http://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E8%8B%B9%E6%9E%9C&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&cs=3058169762,2736869049&os=508208126,2953768013&pn=3&rn=1&di=156292703630&ln=1948&fr=&fmq=1442386142859_R&ic=undefined&s=undefined&se=&sme=&tab=0&width=&height=&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=10&gsm=0&objurl=http%3A%2F%2Fpic.baike.soso.com%2Fp%2F20140126%2F20140126160937-284473768.jpg',
-                    "target_url": location.href.toString(),
+                    "target_url": location.href.toString().replace('index.html', 'invitepage.html') + '?username=' + Base64.encodeURI('啊测试测试') + "&invitecode=JLK789",
                     "target_url_forQQ": location.href.toString()
                 });
             });
         }
 
-        function dAjax() {
-            var userId = null;
+        function doLottery() {
+            $.getJSON(
+                'http://192.168.0.164:9091/app/share/spreadServlet' + "?user_id=" + userId + "&method=" + 'myLottery' + '&callback=?',
+                function (data) {
+                    console.log(data);
+                    $('.top .top-text span.count').text(data._APP_RESULT_OPT_DATA.count);
+                    $('.mask .dialogGetAward').removeClass('none');
+                    $('.mask .dialogGetAward .specialStyle').text(data._APP_RESULT_OPT_DATA.lottery);
+                    $('.mask').removeClass('none');
+                }
+            );
+        }
+
+        function boyBtnBindEv() {
+            $('.boy,.board').on('touchstart', function () {
+                $('.board')[0].src = $('.board')[0].src.toString().replace('board.png', 'board_hover.png');
+            });
+            $('.boy,.board').on('touchend', function () {
+                $('.board')[0].src = $('.board')[0].src.toString().replace('board_hover.png', 'board.png');
+                $('.top .apple.hover').eq('0').addClass('drop').removeClass('hover');
+                setTimeout(function () {
+                    doLottery();
+                }, 500);
+            });
+        }
+
+        function getInviteCode() {
             try {
                 userId = common.getReqPrm('parameter') ? JSON.parse(decodeURIComponent(common.getReqPrm('parameter'))).user_id : null;
             } catch (e) {
                 console.log(e);
             }
             console.log(userId);
-            setTimeout(function () {
-                //$('.mask .dialogGotInvite').removeClass('none');
-                //$('.mask .dialogInviteSuccess').removeClass('none');
-                $('.mask .dialogGetAward').removeClass('none');
-                $('.mask').removeClass('none');
-            }, 500);
+
+            $.getJSON(
+                'http://192.168.0.164:9091/app/share/spreadServlet' + "?user_id=" + userId + "&method=" + 'myCount' + '&callback=?',
+                function (data) {
+                    $('.bottom p.row2 .invitecode').text(data._APP_RESULT_OPT_DATA.code);
+                    $('.top .top-text span.count').text(data._APP_RESULT_OPT_DATA.count);
+                    for (var i = 0; i < data._APP_RESULT_OPT_DATA.count; i++) {
+                        $('.top .apple').eq(i).addClass('hover');
+                    }
+
+                    if (data._APP_RESULT_OPT_DATA.count == 0) {
+                        $('.dialogGotInvite').removeClass('none');
+                        $('.mask').removeClass('none');
+                    }
+                }
+            );
         }
 
         function init() {
             JSNativeBridgeBindEv();
-            dAjax();
+            getInviteCode();
             maskBindEv();
             shareBtnBindEv();
+            boyBtnBindEv();
         }
 
         return {'init': init};
