2480ae15fc5f4b6fd67d2e79da094f475d6dd4fb
Evan Yan <1370204201@qq.com>
2015-09-29 18:56:55 +0800

    toy code commit

--- flask/microblog/app/forms.py
+++ flask/microblog/app/forms.py
@@ -1,5 +1,8 @@
 __author__ = 'yantianyu'
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 from flask_wtf import Form
 from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
@@ -7,4 +10,8 @@ from wtforms.validators import DataRequired
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
+<<<<<<< Updated upstream
     remember_me = BooleanField('remember_me', default=False)
+=======
+    remember_me = BooleanField('remember_me', default=False)
+>>>>>>> Stashed changes
--- flask/microblog/config.py
+++ flask/microblog/config.py
@@ -1,8 +1,11 @@
 __author__ = 'yantianyu'
+<<<<<<< Updated upstream
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+=======
+>>>>>>> Stashed changes
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -12,6 +15,9 @@ OPENID_PROVIDERS = [
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+<<<<<<< Updated upstream
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+=======
+>>>>>>> Stashed changes
