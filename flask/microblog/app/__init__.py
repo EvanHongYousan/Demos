b5fcb655703cb17fa1e6edf7c91a371f97c30f1e
Evan Yan <1370204201@qq.com>
2015-09-30 18:33:18 +0800

    toy code commit

--- flask/microblog/app/__init__.py
+++ flask/microblog/app/__init__.py
@@ -4,9 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-<<<<<<< Updated upstream
 db = SQLAlchemy(app)
 from app import views, models
-=======
-from app import views
->>>>>>> Stashed changes
