3b0f77360c5220cd8b7e3d9c22bbdf9bff9e41fc
Evan Yan <1370204201@qq.com>
2015-09-30 16:16:11 +0800

    toy code commit

--- scratch-paper/swipe_scratch.js
+++ scratch-paper/swipe_scratch.js
@@ -53,7 +53,7 @@ function Swipe(container, options) {
             slide.setAttribute('data-index', pos);
 
             if (browser.transitions) {
-                slides.style.left = (pos * -width) + 'px';
+                slide.style.left = (pos * -width) + 'px';
                 move(pos, index > pos ? -width : (index < pos ? width : 0), 0);
             }
         }
@@ -113,4 +113,321 @@ function Swipe(container, options) {
         index = to;
         offloadFn(options.callback && options.callback(index, slides[index]));
     }
+
+    function move(index, dist, speed) {
+        translate(index, dist, speed);
+        slidePos[index] = dist;
+    }
+
+    function translate(index, dist, speed) {
+        var slide = slides[index];
+        var style = slide && slide.style;
+
+        if (!style) return;
+
+        style.webkitTransitionDuration =
+            style.MozTransitionDuration =
+                style.msTransitionDuration =
+                    style.OTransitionDuration =
+                        style.transitionDuration = speed + 'ms';
+
+        style.webkitTransform = 'translate(' + dist + 'px,0)' + 'translateZ(0)';
+        style.msTransform =
+            style.MozTransform =
+                style.OTransform = 'translateX(' + dist + 'px)';
+    }
+
+    function animate(from, to, speed) {
+        if (!speed) {
+            element.style.left = to + 'px';
+            return;
+        }
+
+        var start = +new Date;
+
+        var timer = setInterval(function () {
+            var timeElap = +new Date - start;
+            if (timeElap > speed) {
+                element.style.left = to + 'px';
+                if (delay) begin();
+                options.transitionEnd && options.transitionEnd.call(event, index, slides[index]);
+                clearInterval(timer);
+                return;
+            }
+            element.style.left = (((to - from) * (Math.floor((timeElap / speed) * 100) / 100)) + from) + 'px';
+        }, 4);
+    }
+
+    var delay = options.auto || 0;
+    var interval;
+
+    function begin() {
+        interval = setTimeout(next, delay);
+    }
+
+    function stop() {
+        delay = 0;
+        clearTimeout(interval);
+    }
+
+    var start = {};
+    var delta = {};
+    var isScrolling;
+
+    var events = {
+        hanleEvent: function (event) {
+            switch (event.type) {
+                case 'touchstart':
+                    this.start(event);
+                    break;
+                case 'touchmove':
+                    this.move(event);
+                    break;
+                case 'touchend':
+                    offloadFn(this.end(event));
+                    break;
+                case 'webkitTransitionEnd':
+                case 'msTransitionEnd':
+                case 'oTransitionEnd':
+                case 'otransitionend':
+                case 'transitionend':
+                    offloadFn(this.transitionEnd(event));
+                    break;
+                case 'resize':
+                    offloadFn(setup);
+                    break;
+            }
+            if (options.stopProgapation)event.stopPropagation();
+        },
+        start: function (event) {
+            var touches = event.touches[0];
+
+            start = {
+                x: touches.pageX,
+                y: touches.pageY,
+                time: +new Date
+            };
+
+            isScrolling = undefined;
+
+            delta = {};
+
+            element.addEventListener('touchmove', this, false);
+            element.addEventListener('touchend', this, false);
+        },
+        move: function (event) {
+            if (event.touches.length > 1 || event.scale && event.scale !== 1)return;
+            if (options.disableScroll) event.preventDefault();
+
+            var touches = event.touches[0];
+
+            delta = {
+                x: touches.pageX - start.x,
+                y: touches.pageY - start.y
+            };
+
+            if (typeof isScrolling == 'undefined') {
+                isScrolling = !!(isScrolling || Math.abs(delta.x) < Math.abs(delta.y));
+            }
+
+            if (!isScrolling) {
+                event.preventDefault();
+
+                stop();
+
+                if (options.continuous) {
+                    translate(circle(index - 1), delta.x + slidePos[circle(index - 1)], 0);
+                    translate(index, delta.x + slidePos[index], 0);
+                    translate(circle(index + 1), delta.x + slidePos[circle(index + 1)], 0);
+                } else {
+                    delta.x =
+                        delta.x /
+                        ((!index && delta.x > 0
+                        || index == slides.length - 1
+                        && delta.x < 0) ?
+                            (Math.abs(delta.x) / width + 1)
+                            : 1);
+
+                    translate(index - 1, delta.x + slidePos[index - 1], 0);
+                    translate(index, delta.x + slidePos[index], 0);
+                    translate(index + 1, delta.x + slidePos[index + 1], 0);
+                }
+            }
+        },
+        end: function (event) {
+
+            // measure duration
+            var duration = +new Date - start.time;
+
+            // determine if slide attempt triggers next/prev slide
+            var isValidSlide =
+                Number(duration) < 250               // if slide duration is less than 250ms
+                && Math.abs(delta.x) > 20            // and if slide amt is greater than 20px
+                || Math.abs(delta.x) > width / 2;      // or if slide amt is greater than half the width
+
+            // determine if slide attempt is past start and end
+            var isPastBounds =
+                !index && delta.x > 0                            // if first slide and slide amt is greater than 0
+                || index == slides.length - 1 && delta.x < 0;    // or if last slide and slide amt is less than 0
+
+            if (options.continuous) isPastBounds = false;
+
+            // determine direction of swipe (true:right, false:left)
+            var direction = delta.x < 0;
+
+            // if not scrolling vertically
+            if (!isScrolling) {
+
+                if (isValidSlide && !isPastBounds) {
+
+                    if (direction) {
+
+                        if (options.continuous) { // we need to get the next in this direction in place
+
+                            move(circle(index - 1), -width, 0);
+                            move(circle(index + 2), width, 0);
+
+                        } else {
+                            move(index - 1, -width, 0);
+                        }
+
+                        move(index, slidePos[index] - width, speed);
+                        move(circle(index + 1), slidePos[circle(index + 1)] - width, speed);
+                        index = circle(index + 1);
+
+                    } else {
+                        if (options.continuous) { // we need to get the next in this direction in place
+
+                            move(circle(index + 1), width, 0);
+                            move(circle(index - 2), -width, 0);
+
+                        } else {
+                            move(index + 1, width, 0);
+                        }
+
+                        move(index, slidePos[index] + width, speed);
+                        move(circle(index - 1), slidePos[circle(index - 1)] + width, speed);
+                        index = circle(index - 1);
+
+                    }
+
+                    options.callback && options.callback(index, slides[index]);
+
+                } else {
+
+                    if (options.continuous) {
+
+                        move(circle(index - 1), -width, speed);
+                        move(index, 0, speed);
+                        move(circle(index + 1), width, speed);
+
+                    } else {
+
+                        move(index - 1, -width, speed);
+                        move(index, 0, speed);
+                        move(index + 1, width, speed);
+                    }
+
+                }
+
+            }
+
+            // kill touchmove and touchend event listeners until touchstart called again
+            element.removeEventListener('touchmove', events, false)
+            element.removeEventListener('touchend', events, false)
+
+        },
+        transitionEnd: function (event) {
+            if (parseInt(event.target.getAttribute('data-index'), 10) == index) {
+                if (delay) begin();
+                options.transitionEnd && options.transitionEnd.call(event.index, slides[index]);
+            }
+        }
+    };
+
+    setup();
+
+    if (delay) begin();
+
+    if (browser.addEventListener) {
+        if (browser.touch) element.addEventListener('touchstart', events, false);
+
+        if (browser.transitions) {
+            element.addEventListener('webkitTransitionEnd', events, false);
+            element.addEventListener('msTransitionEnd', events, false);
+            element.addEventListener('oTransitionEnd', events, false);
+            element.addEventListener('otransitionend', events, false);
+            element.addEventListener('transitionend', events, false);
+        }
+
+        window.addEventListener('resize', events, false);
+    } else {
+        window.onresize = function () {
+            setup();
+        }
+    }
+
+    return {
+        setup: function () {
+            setup();
+        },
+        slide: function (to, speed) {
+            stop();
+            slide(to, speed);
+        },
+        prev: function () {
+            stop();
+            prev();
+        },
+        next: function () {
+            stop();
+            next();
+        },
+        stop: function () {
+            stop();
+        },
+        getPos: function () {
+            return index;
+        },
+        getNumSlides: function () {
+            return length;
+        },
+        kill: function () {
+            stop();
+            element.style.width = '';
+            element.style.left = '';
+
+            var pos = slides.length;
+            while (pos--) {
+                var slide = slides[pos];
+                slide.style.width = '';
+                slide.style.left = '';
+                if (browser.transitions) translate(pos, 0, 0);
+            }
+
+            if (browser.addEventListener) {
+                element.removeEventListener('touchstart', events, false);
+                element.removeEventListener('webkitTransitionEnd', events, false);
+                element.removeEventListener('msTransitionEnd', events, false);
+                element.removeEventListener('oTransitionEnd', events, false);
+                element.removeEventListener('otransitionend', events, false);
+                element.removeEventListener('transitionend', events, false);
+                element.removeEventListener('otransitionend', events, false);
+                element.removeEventListener('resize', events, false);
+            }
+            else {
+                window.onresize = null;
+            }
+        }
+    }
+}
+
+if (window.jQuery || window.Zepto) {
+    (function ($) {
+        $.fn.Swipe = function (params) {
+            return this.each(function () {
+                $(this).data('Swipe', new Swipe($(this)[0], params));
+            })
+        }
+    })(window.jQuery || window.Zepto)
 }
