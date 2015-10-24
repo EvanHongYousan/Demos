b00dbfecad4c38ee064fb9f85d33ccaa9378ca50
Evan Yan <1370204201@qq.com>
2015-10-24 10:27:38 +0800

    toy code commit

---
+++ liaoxuefeng-tutorial/hashlibTest.py
@@ -0,0 +1,10 @@
+__author__ = 'yantianyu'
+import hashlib
+
+md5 = hashlib.md5()
+md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
+print(md5.hexdigest())
+
+sha1 = hashlib.sha1()
+sha1.update('how to use md5 in python hashlib?'.encode('utf-8'))
+print(sha1.hexdigest())
