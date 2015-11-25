8b9709b8913fe743fd7eb39189e6e3c5c301973c
EvanHongYousan <1370204201@qq.com>
2015-11-25 10:20:52 +0800

    整体样式初步构建完毕

--- teacherOnline/star-project/js/main.js
+++ teacherOnline/star-project/js/main.js
@@ -1,3 +1,14 @@
 /**
  * Created by yantianyu on 2015/11/23.
  */
+
+setInterval(function(){
+    $('.scrollDiv').append('<p>恭喜<span class="name">梁春</span>抽中了<span class="award">“Apple Watch”</span></p>');
+    $('.scrollDiv').scrollTo({
+        endY: $('.scrollDiv')[0].scrollHeight,
+        duration: 1000,
+        callback: function () {
+            console.log($('.scrollDiv')[0].scrollHeight);
+        }
+    });
+},1500);
