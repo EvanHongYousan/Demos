a887859d99579377d6fc229ad804ee7594dde0c3
EvanHongYousan <1370204201@qq.com>
2015-11-26 17:18:49 +0800

    活动页阶段性commit

--- teacherOnline/star-project/js/main.js
+++ teacherOnline/star-project/js/main.js
@@ -2,6 +2,7 @@
  * Created by yantianyu on 2015/11/23.
  */
 
+/*animate test start*/
 setInterval(function(){
     $('.scrollDiv').append('<p>恭喜<span class="name">梁春</span>抽中了<span class="award">“Apple Watch”</span></p>');
     $('.scrollDiv').scrollTo({
@@ -12,3 +13,42 @@ setInterval(function(){
         }
     });
 },1500);
+
+globalI = 1;
+setInterval(function(){
+    if(globalI == 13){
+        globalI = 1;
+    }
+    $('.container .dialContainer  span').removeClass('active');
+    $('.container .dialContainer  span.item'+globalI).addClass('active');
+    globalI++;
+},100);
+/*animate test end*/
+
+var components = (function(){
+    function maskInit(){
+        $('.mask').on('touchmove',function(){
+            return false;
+        })
+    }
+
+    function activityRulesBindEv(){
+        $('.activity-rules').on('click',function(){
+            $('.mask, .mask .activityRulesAlert').removeClass('none');
+        });
+        $('.activityRulesAlertBtn').on('click', function(){
+            $('.mask, .mask .activityRulesAlert').addClass('none');
+        })
+    }
+
+    function init(){
+        maskInit();
+        activityRulesBindEv();
+    }
+
+    return {
+        init:init
+    };
+})();
+
+components.init();
