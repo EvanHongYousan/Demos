5a36c3a3e0441d9838483f50b8baf66ab77f7b6d
EvanHongYousan <1370204201@qq.com>
2015-11-30 10:25:44 +0800

    抽奖转盘，抽奖时图片变亮

--- teacherOnline/star-project/js/main.js
+++ teacherOnline/star-project/js/main.js
@@ -3,55 +3,103 @@
  */
 
 /*animate test start*/
-setInterval(function(){
+setInterval(function () {
     $('.scrollDiv').append('<p>恭喜<span class="name">梁春</span>抽中了<span class="award">“Apple Watch”</span></p>');
     $('.scrollDiv').scrollTo({
         endY: $('.scrollDiv')[0].scrollHeight,
         duration: 1000,
         callback: function () {
-            console.log($('.scrollDiv')[0].scrollHeight);
+            //console.log($('.scrollDiv')[0].scrollHeight);
         }
     });
-},1500);
+}, 1500);
 
 globalI = 1;
-setInterval(function(){
-    if(globalI == 13){
+timePick = 50;
+function goRotate() {
+    if (timePick > 800) {
+        return;
+    }
+    if (globalI == 13) {
         globalI = 1;
     }
     $('.container .dialContainer  span').removeClass('active');
-    $('.container .dialContainer  span.item'+globalI).addClass('active');
+    $('.container .dialContainer  span.item' + globalI).addClass('active');
     globalI++;
-},100);
+    timePick += 50;
+    setTimeout('goRotate()', timePick);
+}
+goRotate();
 /*animate test end*/
 
-var components = (function(){
-    function maskInit(){
-        $('.mask').on('touchmove',function(){
+var components = (function () {
+    function maskInit() {
+        $('.mask').on('touchmove', function () {
             return false;
         })
     }
 
-    function activityRulesBindEv(){
-        $('.activity-rules').on('click',function(){
-            $('.mask, .mask .activityRulesAlert').removeClass('none');
+    function canvasCreate() {
+        $('.dialContainer span img').each(function () {
+            this.onload = function () {
+                var $canvas = $('<canvas></canvas>');
+                $canvas.height($(this).height());
+                $canvas.width($(this).width());
+                $(this).parent().append($canvas);
+            }
         });
     }
 
-    function closeBtnBindEv(){
-        $('.closeBtn').on('click', function(){
-            $('.mask, .mask > div').addClass('none');
+    function maskShow(dialog) {
+        $('.mask, .mask .' + dialog).removeClass('none');
+        $('body').css('overflow', 'hidden');
+    }
+
+    function maskHide(){
+        $('.mask, .mask > div').addClass('none');
+        $('body').css('overflow','auto');
+    }
+
+    function activityRulesBindEv() {
+        $('.activity-rules').on('click', function () {
+            maskShow('activityRulesAlert');
         });
     }
 
-    function init(){
-        maskInit();
+    function closeBtnBindEv() {
+        $('.closeBtn').on('click', function () {
+            maskHide();
+        });
+    }
+
+    function showAwardRecordBtnBindEv(){
+        $('.showAwardRecord').on('click',function(){
+            maskShow('drawRecordDialog');
+        })
+    }
+
+    function bindPhoneNumberBtnBindEv() {
+        $('.bindPhoneNumberBtn').on('click', function () {
+            JSNativeBridge.send('js_msg_bind_phonenumber');
+        })
+    }
+
+    function JSNativeBridgeBindEv() {
+        JSNativeBridge.init();
+    }
+
+    function init() {
+        //maskInit();
         activityRulesBindEv();
         closeBtnBindEv();
+        bindPhoneNumberBtnBindEv();
+        JSNativeBridgeBindEv();
+        showAwardRecordBtnBindEv();
+        //canvasCreate();
     }
 
     return {
-        init:init
+        init: init
     };
 })();
 
