9b27711576683b7975edee322fb82f9cdb7dfa0e
EvanHongYousan <1370204201@qq.com>
2015-12-31 18:35:45 +0800

    细节调整

--- teacherOnline/RedPacketActivity-NY/common/spread.js
+++ teacherOnline/RedPacketActivity-NY/common/spread.js
@@ -1,11 +1,11 @@
 ﻿var spreadData = {
-	id : '56849a200cf20005a2b5a0f0',
-	name:["拼手气，抢期末助考红包","拼手气，抢期末助考红包","拼手气，抢期末助考红包"],
+	id : '5684aa280cf20005a2b5a0f7',
+	name:["过年啦~！送你一个新年红包。","压岁钱不够花？来领新年红包！","恭喜发财，红包送你！"],
 	pic : 'http://test.hjlaoshi.com/rtc/spread/shareicon.png',
 	jsonPURL : 'http://test.hjlaoshi.com/app/spread/activity',
 	shareURL_forQQ : 'http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-NY/share/direct.html',
 	shareURL : 'http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-NY/share/index.html',
 	beginTime:"2015-12-04 19:05:00",
 	endTime:"2016-12-31 19:05:00",
-	shareWording : ['红包送你，免费问老师，助你考试考高分~！','红包送你，免费问老师，助你考试考高分~！','红包送你，免费问老师，助你考试考高分~！']
+	shareWording : ['呼叫老师，发新年红包啦~祝小伙伴新的一年，天天开心，越来越COOL~','妈妈再也不用担心我的压岁钱不够啦~','呼叫老师，恭贺新禧，100万压岁钱随便来领~']
 };
