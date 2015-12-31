3156e0c7f7133a3f30b3dda851a578ea72444781
EvanHongYousan <1370204201@qq.com>
2015-12-31 14:34:08 +0800

    红包活动新年版，目录调整。

---
+++ teacherOnline/RedPacketActivity-NY/common/spread.js
@@ -0,0 +1,11 @@
+﻿var spreadData = {
+	id : '56849a200cf20005a2b5a0f0',
+	name:["拼手气，抢期末助考红包","拼手气，抢期末助考红包","拼手气，抢期末助考红包"],
+	pic : 'http://test.hjlaoshi.com/rtc/spread/shareicon.png',
+	jsonPURL : 'http://test.hjlaoshi.com/app/spread/activity',
+	shareURL_forQQ : 'http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-NY/share/direct.html',
+	shareURL : 'http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-NY/share/index.html',
+	beginTime:"2015-12-04 19:05:00",
+	endTime:"2016-12-31 19:05:00",
+	shareWording : ['红包送你，免费问老师，助你考试考高分~！','红包送你，免费问老师，助你考试考高分~！','红包送你，免费问老师，助你考试考高分~！']
+};
