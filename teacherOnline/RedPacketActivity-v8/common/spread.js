d088862f048929bae6e4b1d7c0d5aa227b145d53
Evan Yan <1370204201@qq.com>
2016-03-07 09:36:51 +0800

    toy code commit

---
+++ teacherOnline/RedPacketActivity-v8/common/spread.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yantianyu on 15/8/20.
+ */
+var spreadData = {
+    id : '56d943b50cf248cc2108cbe8',
+    name: ["快来抢，100万开学红包等你领！", "快来抢，100万开学红包等你领！", "快来抢，100万开学红包等你领！"],
+    pic : 'http://test.hjlaoshi.com/rtc/spread/RedPacketActivity-v8/sharePic.png?v=9909',
+    jsonPURL : 'http://test.hjlaoshi.com/app/spread/activity',
+    shareURL_forQQ : 'http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-v8/share/index.html',
+    shareURL: "http://testftp.hjlaoshi.com/rtc/spread/RedPacketActivity-v8/share/index.html",
+    beginTime: "2016-03-01 00:00:00",
+    endTime: "2016-03-15 00:00:00",
+    shareWording : ['作业堆积成山也不用怕。有呼叫老师，随时问，立刻答，包你懂！', '作业堆积成山也不用怕。有呼叫老师，随时问，立刻答，包你懂！','作业堆积成山也不用怕。有呼叫老师，随时问，立刻答，包你懂！']
+};
