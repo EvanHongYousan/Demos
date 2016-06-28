5f2a74bb64f34a8fa3ee8f09eb16b5476d81d682
Evan Yan <1370204201@qq.com>
2016-06-28 10:10:17 +0800

    toy code commit

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3021;
 
-app.use(express.static(__dirname+'/'));
+app.use(express.static(__dirname+'/teacherOnline/canvasVideo-phone'));
 
 app.listen(port,function(){
     console.log('app is listening at port ' + port);
+    console.log('http://localhost:3021');
 });
